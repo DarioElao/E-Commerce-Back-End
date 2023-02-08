f4fa94e075ead15e499173fd2f11d7c216856686
darioelao <darioelao@gmail.com>
2023-02-08 01:16:07 -0500

    fixed error in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product, product, Tag, ProductTag } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
@@ -9,7 +9,7 @@ router.get('/',async (req, res) => {
   // be sure to include its associated product and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: product}, {model: Tag}]
+      include: [{ model: Category}, {model: Tag}]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated product and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{model: product}, {model: Tag}],
+      include: [{model: Category}, {model: Tag}],
     });
 
     if (!productData) {
