447830d981a95377b6668eba89a856efa957c5e9
darioelao <darioelao@gmail.com>
2023-02-08 00:41:10 -0500

    added routes to product-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }, { attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}],
+      include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
   } catch (err) {
